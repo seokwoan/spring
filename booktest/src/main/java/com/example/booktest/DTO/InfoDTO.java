f7df063a80bf2176ba53fd0b5f867d9eb3553f43
seokwoan <suporterseo@gmail.com>
2024-08-07 12:49:26 +0900

    0807점심

---
+++ booktest/src/main/java/com/example/booktest/DTO/InfoDTO.java
@@ -0,0 +1,13 @@
+package com.example.booktest.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class InfoDTO { //home.jsp의 form데이터를 받기
+
+	private String uName;
+	private String uBirth;
+	
+}
---
+++ booktest/src/main/java/com/example/booktest/DTO/bookDTO.java
@@ -0,0 +1,14 @@
+package com.example.booktest.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class bookDTO {
+
+	private String title;
+	private String author;
+	private String publisher;
+	
+}
